c99de831b65a810a83e196908de51c999bfd8ad8
Zakaria Ait Ouchrif <zaouchrif@boaholding.com>
2024-10-12 16:08:34 +0100

    setup project with necessary dependencies

---
+++ src/test/java/com/alten/product/management/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.product.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
