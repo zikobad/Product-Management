00e015de4d57cf0b558be1c53f258c167444c0aa
Zakaria Ait Ouchrif <zaouchrif@boaholding.com>
2024-10-12 16:23:29 +0100

    create product entity

---
+++ src/main/java/com/alten/product/management/entity/InventoryStatus.java
@@ -0,0 +1,5 @@
+package com.alten.product.management.entity;
+
+public enum InventoryStatus {
+    INSTOCK, LOWSTOCK, OUTOFSTOCK;
+}
---
+++ src/main/java/com/alten/product/management/entity/Product.java
@@ -0,0 +1,43 @@
+package com.alten.product.management.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String code;
+    private String name;
+    private String description;
+    private String image;
+    private String category;
+    private Double price;
+    private Integer quantity;
+    private String internalReference;
+    private Long shellId;
+    @Enumerated(EnumType.STRING)
+    private InventoryStatus inventoryStatus;
+    private Double rating;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
+
+
+
